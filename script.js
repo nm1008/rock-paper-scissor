a0d9cf95d88cbfa6e42048b2aefd36b9587fbba0
nm1008 <mallari08nikko@yahoo.com>
2023-07-16 13:25:58 +0800

    added a score counter

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+let userScore = 0;
+
 function getComputerChoice() {
     let pick = ['rock', 'paper', 'scissor'];
     return pick[Math.floor(Math.random() * 3)];
-    
 }
 
 function play(playerSelection, computerSelection){
@@ -10,26 +11,28 @@ function play(playerSelection, computerSelection){
     if (userPick === "scissor" && computerSelection === "rock") {
         return "You lose! Rock beats Scissor";
     } else if (userPick === 'scissor' && computerSelection === 'paper'){
+        userScore++;
         return "You win! Scissor beats Paper";
     } else if (userPick === 'rock' && computerSelection === 'paper'){
         return "You lose! Paper beats Rock";
     } else if (userPick === 'rock' && computerSelection === 'scissor') {
+        userScore++;
         return "You win! Rock beats Scissor";
     } else if (userPick === 'paper' && computerSelection === 'rock') {
+        userScore++;
         return "You Win! Paper beats Rock";
     } else if (userPick === 'paper' && computerSelection === 'scissor') {
         return "You lose! Scissor beats Paper";
     } else {
         return "Draw!";
-    }
+    }  
 }
 
 function game() {
     for (let i = 1; i <= 5; i++){
         const computerSelection = getComputerChoice();
         let playerSelection = prompt("Choose: Rock, Paper or Scissor?");
-        alert(play(playerSelection, computerSelection));
+        alert(play(playerSelection, computerSelection));     
     }
+    alert("Your score is " + userScore );
 }
-
-
