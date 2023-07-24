86bb8dfbbb0c48b2fda68db7991f865ccd8a7580
nm1008 <mallari08nikko@yahoo.com>
2023-07-24 10:13:47 +0800

    added button for rock paper scissors / added DOM to buttons

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
+const btnCointainer = document.querySelector('.btn')
+const div = document.createElement('div');
+
+
+
 let userScore = 0;
 
 function getComputerChoice() {
@@ -28,11 +36,25 @@ function play(playerSelection, computerSelection){
     }  
 }
 
-function game() {
-    for (let i = 1; i <= 5; i++){
+function game(choice) {
+   
         const computerSelection = getComputerChoice();
-        let playerSelection = prompt("Choose: Rock, Paper or Scissor?");
+        let playerSelection = choice;
         alert(play(playerSelection, computerSelection));     
-    }
-    alert("Your score is " + userScore );
+   
+    // alert("Your score is " + userScore );
+    div.textContent = `Your score is ${userScore }`
+    btnCointainer.appendChild(div);
 }
+
+btn1.addEventListener('click', () => {
+    game('Rock')
+});
+
+btn2.addEventListener('click', () => {
+    game('Paper')
+});
+
+btn3.addEventListener('click', () => {
+    game('Scissor')
+});
