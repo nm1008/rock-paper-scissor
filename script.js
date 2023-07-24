4e6184a0a435f09f23208aec6ca0cb7d778de302
nm1008 <mallari08nikko@yahoo.com>
2023-07-24 10:52:06 +0800

    added new feature if player or computer reaches 5 will prompt to play again

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
 const btn3 = document.querySelector('.btn3');
-const btnCointainer = document.querySelector('.btn')
-const div = document.createElement('div');
-
-
+const btnContainer = document.querySelector('.btn')
+const userDiv = document.createElement('div');
+const computerDiv = document.createElement('div');
 
 let userScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let pick = ['rock', 'paper', 'scissor'];
@@ -17,11 +17,13 @@ function play(playerSelection, computerSelection){
     let userPick = playerSelection.toLowerCase();
     
     if (userPick === "scissor" && computerSelection === "rock") {
+        computerScore++;
         return "You lose! Rock beats Scissor";
     } else if (userPick === 'scissor' && computerSelection === 'paper'){
         userScore++;
         return "You win! Scissor beats Paper";
     } else if (userPick === 'rock' && computerSelection === 'paper'){
+        computerScore++;
         return "You lose! Paper beats Rock";
     } else if (userPick === 'rock' && computerSelection === 'scissor') {
         userScore++;
@@ -30,10 +32,13 @@ function play(playerSelection, computerSelection){
         userScore++;
         return "You Win! Paper beats Rock";
     } else if (userPick === 'paper' && computerSelection === 'scissor') {
+        computerScore++;
         return "You lose! Scissor beats Paper";
     } else {
         return "Draw!";
     }  
+    
+    
 }
 
 function game(choice) {
@@ -43,8 +48,29 @@ function game(choice) {
         alert(play(playerSelection, computerSelection));     
    
     // alert("Your score is " + userScore );
-    div.textContent = `Your score is ${userScore }`
-    btnCointainer.appendChild(div);
+        userDiv.textContent = `Your score is ${userScore }`
+        btnContainer.appendChild(userDiv);
+
+        computerDiv.textContent = `Computer score is ${computerScore }`
+        btnContainer.appendChild(computerDiv);
+
+        if(userScore === 5 || computerScore === 5){
+            let playAgain = confirm("Do you want to play again?")
+            if (playAgain) {
+                let userScore = 0;
+                let computerScore = 0;
+                userDiv.textContent = "Your score is 0";
+                computerDiv.textContent = "Computer score is 0";
+            } else {
+                alert("GAME OVER!!")
+            }
+          
+        
+            
+        }
+
+        
+      
 }
 
 btn1.addEventListener('click', () => {
