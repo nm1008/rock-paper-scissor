19147f3cf5796dec2d478a4b02d93a5e10c53a9b
nm1008 <mallari08nikko@yahoo.com>
2023-07-31 21:40:29 +0800

    reconfigure script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 //selectors for button
-const btn1 = document.querySelector('.btn1');
-const btn2 = document.querySelector('.btn2');
-const btn3 = document.querySelector('.btn3');
-const btnContainer = document.querySelector('.btn')
+const rockImg = document.querySelector(".rock-link");
+const paperImg = document.querySelector(".paper-link");
+const scissorsImg = document.querySelector(".scissors-link");
 
 // creating div element for move
-const moveDiv = document.querySelector('.move');
-const move = document.createElement('div');
+const move = document.querySelector(".instruction");
 
-const player = document.querySelector('.playerScore')
-const computer = document.querySelector('.computerScore')
+const player = document.querySelector(".player");
+const computer = document.querySelector(".computer");
 
 let userScore = 0;
 let computerScore = 0;
@@ -28,29 +26,29 @@ function play(playerSelection, computerSelection){
     if (userPick === "scissor" && computerSelection === "rock") {
         computerScore++;
         move.textContent = "You lose! Rock beats Scissor";
-        return moveDiv.appendChild(move)
+       
     } else if (userPick === 'scissor' && computerSelection === 'paper'){
         userScore++;
         move.textContent =  "You win! Scissor beats Paper";
-        return moveDiv.appendChild(move)
+       
     } else if (userPick === 'rock' && computerSelection === 'paper'){
         computerScore++;
         move.textContent =  "You lose! Paper beats Rock";
-        return moveDiv.appendChild(move)
+       
     } else if (userPick === 'rock' && computerSelection === 'scissor') {
         userScore++;
         move.textContent =  "You win! Rock beats Scissor";
     } else if (userPick === 'paper' && computerSelection === 'rock') {
         userScore++;
         move.textContent =  "You Win! Paper beats Rock";
-        return moveDiv.appendChild(move)
+       
     } else if (userPick === 'paper' && computerSelection === 'scissor') {
         computerScore++;
         move.textContent =  "You lose! Scissor beats Paper";
-        return moveDiv.appendChild(move)
+       
     } else {
         move.textContent =  "Draw!";
-        return moveDiv.appendChild(move)
+       
     }  
 }
 
@@ -61,9 +59,9 @@ function game(choice) {
         let playerSelection = choice;
         play(playerSelection, computerSelection)
         
-        player.innerHTML = userScore
-        computer.innerHTML = computerScore
-
+        player.innerHTML = `You: ${userScore}`
+        computer.innerHTML = `Computer: ${computerScore}`
+        
     // if the player or computer scores 5 this if statement declares the winner and prompts if the player  wants to play another round if not alerts game over and refreshes the page for 5 seconds
     if (userScore === 5) {
         alert("You Win!")
@@ -91,15 +89,15 @@ function game(choice) {
     }
 }
 
-//event listener for the buttons
-btn1.addEventListener('click', () => {
+// //event listener for the buttons
+rockImg.addEventListener('click', () => {
     game('Rock')
 });
 
-btn2.addEventListener('click', () => {
+paperImg.addEventListener('click', () => {
     game('Paper')
 });
 
-btn3.addEventListener('click', () => {
+scissorsImg.addEventListener('click', () => {
     game('Scissor')
 });
